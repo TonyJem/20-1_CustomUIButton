88ac576cbf78794ada7bea9f8bb6424b54be49fc
TonyJem <a.jemanov@gmail.com>
2021-01-12 23:34:48 +0200

    Add @IBInspectable var "roundButton" to class "RoundButton"

--- MyCustomUIButton/MyCustomUIButton/RoundButton.swift
+++ MyCustomUIButton/MyCustomUIButton/RoundButton.swift
@@ -1,5 +1,13 @@
 import UIKit
 
 @IBDesignable class RoundButton: UIButton {
-
+    
+    
+    @IBInspectable var roundButton: Bool  = false {
+        didSet{
+            if roundButton {
+                layer.cornerRadius = frame.height / 2
+            }
+        }
+    }
 }
