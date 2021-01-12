c9223a28abbc1230d0343a354b80327eb34082dc
TonyJem <a.jemanov@gmail.com>
2021-01-13 00:00:10 +0200

    Revert "Add @IBInspectable var "roundButton" to class "RoundButton""
    This reverts commit 88ac576cbf78794ada7bea9f8bb6424b54be49fc.

--- MyCustomUIButton/MyCustomUIButton/RoundButton.swift
+++ MyCustomUIButton/MyCustomUIButton/RoundButton.swift
@@ -1,13 +1,5 @@
 import UIKit
 
 @IBDesignable class RoundButton: UIButton {
-    
-    
-    @IBInspectable var roundButton: Bool  = false {
-        didSet{
-            if roundButton {
-                layer.cornerRadius = frame.height / 2
-            }
-        }
-    }
+
 }
