52d8a2753f153feb836826f74f483aeef5062346
TonyJem <a.jemanov@gmail.com>
2021-01-13 00:06:56 +0200

    Create @IBInspectable var "roundButton" in class "CircleButton"

--- MyCustomUIButton/MyCustomUIButton/CircleButton.swift
+++ MyCustomUIButton/MyCustomUIButton/CircleButton.swift
@@ -2,4 +2,12 @@ import UIKit
 
 @IBDesignable class CircleButton: UIButton {
 
+    @IBInspectable var roundButton: Bool = false {
+        didSet {
+            if roundButton {
+                layer.cornerRadius = frame.height / 2
+            }
+        }
+    }
+    
 }
